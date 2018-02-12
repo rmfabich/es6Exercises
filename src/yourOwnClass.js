730ebd31d5ae39532f34eec2de455d4b86bbcb71
kramduckner <buckner.a.mark@gmail.com>
2018-02-12 13:34:03 -0700

    Update yourOwnClass.js

--- src/yourOwnClass.js
+++ src/yourOwnClass.js
@@ -1,2 +1,3 @@
 //create a class-subclass structure and implement it using javascript classes.
 //be sure each have at least one method on them.
+//Export the parent class in one file and import the child class in another
